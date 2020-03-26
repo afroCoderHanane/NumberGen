f201415524454d079c126df63ef27f380b4ae5a9
Abdoul Hanane <gbadhanane@gmail.com>
2020-03-26 21:07:36 +0000

    main

--- main.cpp
+++ main.cpp
@@ -5,10 +5,13 @@ using namespace std;
 int main() {
   ofstream push;
   int p, i=0;
+  int n;
   push.open("text.txt");
-  while(i<1000)
+  cout<<"how many randum number did you want to generate?"<< endl;
+  cin>>n;
+  while(i<n)
   { 
-    p =rand()%1000;
+    p =rand()%n;
     push<<p<<" ";
     i++;
   }
