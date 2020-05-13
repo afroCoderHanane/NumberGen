b3532ca005284cdd8cbbfabfb096616eee85a5e7
Abdoul Hanane <gbadhanane@gmail.com>
2020-05-13 13:55:20 +0000

    corrected random

--- main.cpp
+++ main.cpp
@@ -16,6 +16,9 @@ void setsize(int & n)
   
 }
 using namespace std;
+
+/*function that produces random numbers*/
+
 void createFile(){
     ofstream push;
     int p, i = 0;
@@ -24,14 +27,17 @@ void createFile(){
     cout << "how many randum number did you want to generate?" << endl;
     cin >> n;
     setsize(n);
+     srand (time(NULL));
     while (i < n)
     {
+       
         p = rand() % n;
         push << p << " ";
         i++;
     }
     push.close();
 }
+/*function that put numbers in the created file into a dynamically allocated array*/
 void putnumArray(int* array)
 {
     ifstream put;
@@ -57,7 +63,7 @@ void printArray(int *array)
 
 int main()
 {
-    createFile();
+    createFile();//create the random numbers;
     
     int j;
     ofstream push;
@@ -65,9 +71,10 @@ int main()
     int* a_sorted= new int[numsize];
     int* d_sorted= new int[numsize];
     int* c_unsorted = new int[numsize];
+   
+   /*almost sorted*/
     putnumArray(a_sorted);
     sort(a_sorted, a_sorted + numsize*90/100);
-    
     cout << "almost sorted" << endl;
     printArray(a_sorted);
     push.open("a_text.txt", ios::out);
@@ -84,22 +91,21 @@ int main()
     heapSort(a_sorted, numsize);
        print(); //print the swap and the comparisons numbers 
        reset(); // set the counter to 0
-    cout<<endl<<"after Sorting"<<endl;
+    cout<<endl<<"after Sorting the almost sorted array"<<endl;
      printArray(a_sorted);
     cout<< endl;
+
+   /* decreasing order sorted */
+
     putnumArray(d_sorted);
-   
     sort(d_sorted, d_sorted + numsize, greater<int>());
     cout << "descending order sorted" << endl;
+    printArray(a_sorted);
     push.open("d_text.txt", ios::out);
     for (int i = 0; i < numsize; i++)
-    {   
-        cout << d_sorted[i] << " " ;
-        push<<d_sorted[i] << " ";
-    }
+       push<< d_sorted[i] << " " ;
     cout << endl;
     push.close();
-
     //insertionSort(d_sorted, numsize);
     //selectionSort(d_sorted, numsize);
     //mergeSort(d_sorted,0,numsize-1);
@@ -107,19 +113,15 @@ int main()
     heapSort(d_sorted, numsize);
       print(); //print the swap and the comparisons numbers
       reset(); // set the counter to 0
-    cout<<endl<<"after Sorting"<<endl;
-    for (int i = 0; i < numsize; i++)
-    {   
-        cout << d_sorted[i] << " " ;
-        
-    }
+    cout<<endl<<"after Sorting decreasing order sorted array"<<endl;
+    printArray(d_sorted);
     cout<< endl;
+
+   /*completely unsorted array*/
+
     putnumArray (c_unsorted);
     cout << "completely unsorted" << endl;
-    for (int i = 0; i < numsize; i++)
-    {
-        cout << c_unsorted[i] << " " ;
-    }
+    printArray(c_unsorted);
     cout << endl;
     push.close();
     //insertionSort(c_unsorted, numsize);
@@ -129,13 +131,8 @@ int main()
     heapSort(c_unsorted, numsize);
       print();//print the swap and the comparisons numbers
       reset();// set the counter to 0
-    cout<<endl<<"after Sorting"<<endl;
-    for (int i = 0; i < numsize; i++)
-    {   
-        cout << c_unsorted[i] << " " ;
-        
-    }
-
+    cout<<endl<<"after Sorting the completely unsorted array"<<endl;
+   printArray(c_unsorted);
 
 //delete the memory allocation
 delete [] a_sorted;
